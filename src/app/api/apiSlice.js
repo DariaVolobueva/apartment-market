e260fd39661c059ce9853f22647e227a6bb9c239
DariaVolobueva <65347045+DariaVolobueva@users.noreply.github.com>
2024-05-21 01:55:26 +0300

    Update apiSlice.js
    Change baseUrl

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://apartment-market.onrender.com",
+        baseUrl: "https://apartment-market-api.onrender.com",
     }),
     tagTypes: ["Apartment"],
     endpoints: (builder) => ({}),
